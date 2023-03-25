ad1c77ade92fc879203b666090952f13d9fc5c94
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2023-03-25 08:38:15 -0400

    ACTUALLY fix the loading state issue with header

--- components/sections/Header.tsx
+++ components/sections/Header.tsx
@@ -9,26 +9,28 @@ import styles from "@/styles/sections/Header.module.css";
 export default function Header() {
   const { data: session, status } = useSession();
   const loading = status === "loading";
-  console.log(session, status);
+  console.log(session, loading);
 
   return (
     <header className={styles.Header}>
-      <Logo />
+      <div className={!session && loading ? styles.loading : styles.loaded}>
+        <Logo />
 
-      {!session ? (
-        <div>
-          <button className='btn btn-primary' onClick={() => signIn()}>
-            Log In
-          </button>
-        </div>
-      ) : (
-        <div>
-          <button onClick={() => signOut()}>Log Out</button>
-          <div className={styles.profileBackground}>
-            <FontAwesomeIcon className={styles.profile} icon={faUser} />
+        {!session ? (
+          <div>
+            <button className='btn btn-primary' onClick={() => signIn()}>
+              Log In
+            </button>
           </div>
-        </div>
-      )}
+        ) : (
+          <div style={{ display: "flex" }}>
+            <button onClick={() => signOut()}>Log Out</button>
+            <div className={styles.profileBackground}>
+              <FontAwesomeIcon className={styles.profile} icon={faUser} />
+            </div>
+          </div>
+        )}
+      </div>
     </header>
   );
 }
