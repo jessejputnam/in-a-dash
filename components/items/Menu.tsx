1e0df051cc11121f96afd78ee86b853556ae06f1
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2023-03-25 15:52:36 -0400

    Make basic account menu

---
+++ components/items/Menu.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import { signOut } from "next-auth/react";
+
+import styles from "@/styles/items/Menu.module.css";
+
+export function Menu({ select }: any) {
+  return (
+    <ul className={styles.Menu}>
+      <li>
+        <Link onClick={() => select()} className={styles.hover} href='#'>
+          Profile
+        </Link>
+      </li>
+      <hr />
+      <li>
+        <Link onClick={() => select()} className={styles.hover} href='#'>
+          Settings
+        </Link>
+      </li>
+      <hr />
+      <li>
+        <Link onClick={() => select()} className={styles.hover} href='#'>
+          Dark Mode
+        </Link>
+      </li>
+      <hr />
+      <li>
+        <p
+          className={styles.hover}
+          onClick={() => {
+            signOut();
+            select();
+          }}
+        >
+          Log Out
+        </p>
+      </li>
+    </ul>
+  );
+}
--- components/sections/Header.tsx
+++ components/sections/Header.tsx
@@ -1,15 +1,19 @@
+import { useState } from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 import Logo from "@/components/items/Logo";
+import { Menu } from "../items/Menu";
 
 import styles from "@/styles/sections/Header.module.css";
 
 export default function Header() {
   const { data: session, status } = useSession();
+  const [menu, setMenu] = useState(false);
   const loading = status === "loading";
-  console.log(session, loading);
+
+  const handleSelect = () => setMenu(false);
 
   return (
     <header className={styles.Header}>
@@ -23,14 +27,15 @@ export default function Header() {
             </button>
           </div>
         ) : (
-          <div style={{ display: "flex" }}>
-            <button onClick={() => signOut()}>Log Out</button>
-            <div className={styles.profileBackground}>
-              <FontAwesomeIcon className={styles.profile} icon={faUser} />
-            </div>
+          <div
+            className={styles.profileBackground}
+            onClick={() => setMenu(!menu)}
+          >
+            <FontAwesomeIcon className={styles.profile} icon={faUser} />
           </div>
         )}
       </div>
+      {menu ? <Menu select={handleSelect} /> : null}
     </header>
   );
 }
