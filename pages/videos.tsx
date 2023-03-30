4c7314c55ef62ee3d6dcc8e3f3bd886720cae75a
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2023-03-30 19:38:45 -0400

    Fix VideosPage style import

--- pages/videos.tsx
+++ pages/videos.tsx
@@ -1,12 +1,12 @@
 import Layout from "@/components/Layout";
 import Videos from "@/components/sections/Videos";
 
-import styles from "@/styles/";
+import styles from "@/styles/pages/Videos.module.css";
 
 export default function VideosPage() {
   return (
     <Layout>
-      <div className={"VideosPage"}>
+      <div className={styles.VideosPage}>
         <h1>Videos</h1>
         <p>Discover the latest on YouTube</p>
         <Videos />
