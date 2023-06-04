b2ef0bf1fffe6c634b8434c083c63b251c581f7f
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2023-06-04 09:49:22 -0400

    Convert Logo to styled components

--- components/items/BurgerNav.tsx
+++ components/items/BurgerNav.tsx
@@ -1,32 +1,54 @@
 import Link from "next/link";
+import styled from "styled-components";
 
-import styles from "@/styles/items/BurgerNav.module.css";
+const Nav = styled.ul`
+  position: absolute;
+  right: 0;
+  top: 61px;
+  width: 100%;
+  height: 300px;
+  background-color: var(--gray);
+  box-shadow: -1px 2px 3px 0 var(--dark-box-shadow);
+  z-index: 1000;
+
+  @media only screen and (min-width: 600px) {
+    display: none;
+  }
+`;
+
+const Li = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: var(--dark-gray);
+  font-weight: 300;
+  font-size: 2rem;
+  padding: 31px 25px;
+
+  & :hover {
+    color: var(--red);
+  }
+`;
 
 function BurgerNav({ select }: { select: () => void }) {
   return (
-    <ul className={styles.BurgerNav}>
-      <li onClick={() => select()}>
-        <Link className={styles.link} href='/articles'>
-          Articles
-        </Link>
-      </li>
+    <Nav>
+      <Li onClick={() => select()}>
+        <Link href='/articles'>Articles</Link>
+      </Li>
 
       <hr />
 
-      <li onClick={() => select()}>
-        <Link className={styles.link} href='/videos'>
-          Videos
-        </Link>
-      </li>
+      <Li onClick={() => select()}>
+        <Link href='/videos'>Videos</Link>
+      </Li>
 
       <hr />
 
-      <li onClick={() => select()}>
-        <Link className={styles.link} href='/posts'>
-          Posts
-        </Link>
-      </li>
-    </ul>
+      <Li onClick={() => select()}>
+        <Link href='/posts'>Posts</Link>
+      </Li>
+    </Nav>
   );
 }
 
--- components/items/Logo.tsx
+++ components/items/Logo.tsx
@@ -1,16 +1,89 @@
-import styles from "@/styles/items/Logo.module.css";
+import styled from "styled-components";
 import Link from "next/link";
+
+const P = styled.p`
+  color: white;
+  font-size: 68px;
+  font-family: var(--font-cursive);
+  height: 100%;
+  position: absolute;
+  bottom: 15px;
+  left: 8px;
+  transform: rotate(5.5deg);
+`;
+
+const Clip = styled.div`
+  background-color: var(--red);
+  position: absolute;
+  width: 70px;
+  height: 8px;
+  z-index: 2;
+`;
+
+const Bar = styled.div`
+  height: 13px;
+  background-color: white;
+  position: absolute;
+`;
+
+const Div = styled.div`
+  background-color: var(--red);
+  height: 60px;
+  width: 70px;
+  position: relative;
+  :hover {
+    cursor: pointer;
+    background-color: white;
+  }
+  &:hover {
+    ${Clip} {
+      background-color: white;
+    }
+
+    ${Bar} {
+      background-color: var(--red);
+    }
+
+    ${P} {
+      color: var(--red);
+    }
+  }
+`;
+
+const Top = styled(Bar)`
+  width: 43px;
+  top: 7.8px;
+  -webkit-clip-path: polygon(0% 0%, 100% 0%, 75% 100%, 0% 100%);
+  -moz-clip-path: polygon(0% 0%, 100% 0%, 75% 100%, 0% 100%);
+  clip-path: polygon(0% 0%, 100% 0%, 75% 100%, 0% 100%);
+`;
+
+const Mid = styled(Bar)`
+  width: 31px;
+  top: 22.9px;
+  -webkit-clip-path: polygon(0% 0%, 100% 0%, 66% 100%, 0% 100%);
+  -moz-clip-path: polygon(0% 0%, 100% 0%, 66% 100%, 0% 100%);
+  clip-path: polygon(0% 0%, 100% 0%, 66% 100%, 0% 100%);
+`;
+
+const Low = styled(Bar)`
+  width: 19px;
+  top: 37.9px;
+  -webkit-clip-path: polygon(0% 0%, 100% 0%, 49% 100%, 0% 100%);
+  -moz-clip-path: polygon(0% 0%, 100% 0%, 49% 100%, 0% 100%);
+  clip-path: polygon(0% 0%, 100% 0%, 48% 100%, 0% 100%);
+`;
 
 export default function Logo() {
   return (
-    <Link href='/' className={styles.Logo}>
-      <div className={styles.clipBar}></div>
-      <div className={styles.barBox}>
-        <div className={`${styles.bar} ${styles.top}`}></div>
-        <div className={`${styles.bar} ${styles.mid}`}></div>
-        <div className={`${styles.bar} ${styles.low}`}></div>
-      </div>
-      <p>D</p>
+    <Link href='/'>
+      <Div>
+        <Clip></Clip>
+        <Top></Top>
+        <Mid></Mid>
+        <Low></Low>
+        <P>D</P>
+      </Div>
     </Link>
   );
 }
