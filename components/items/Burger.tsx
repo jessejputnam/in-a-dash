cca8e9670cd1a4e8917786a6c3f5e69b714a3527
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2023-06-04 09:00:30 -0400

    Convert burger to styled components

--- components/items/Burger.tsx
+++ components/items/Burger.tsx
@@ -1,30 +1,69 @@
-import Link from "next/link";
+import styled from "styled-components";
+const mobile = "601px";
+const boxSize = "60px";
+const delay = "0.2s";
+const barSize = "5px";
+const barColor = "black";
 
-import styles from "@/styles/items/Burger.module.css";
+const Box = styled.div`
+  position: relative;
+  width: ${boxSize};
+  height: ${boxSize};
+  @media only screen and (min-width: ${mobile}) {
+    display: none;
+  }
+`;
 
-function Burger({ toggle, open }: { open: boolean; toggle: () => void }) {
+const Bar = styled.div<{ $open: boolean }>`
+  width: ${boxSize};
+  height: ${barSize};
+  background-color: ${barColor};
+  border-radius: 5px;
+  position: absolute;
+`;
+
+const Bun = styled(Bar)<{ $open: boolean }>`
+  opacity: ${(props) => (props.$open ? "0" : "1")};
+  transition: top ${delay}, opacity 0s;
+  transition-delay: ${(props) => (props.$open ? "0s" : delay)}, ${delay};
+  transition-timing-function: ease-in-out, linear;
+`;
+
+const TopBun = styled(Bun)<{ $open: boolean }>`
+  top: ${(props) => (props.$open ? "45%" : "15%")};
+`;
+
+const MidBun = styled(Bun)`
+  top: 45%;
+`;
+
+const LowBun = styled(Bun)<{ $open: boolean }>`
+  top: ${(props) => (props.$open ? "45%" : "75%")};
+`;
+
+const BarX = styled(Bar)<{ $open: boolean }>`
+  top: 45%;
+  transition: transform ${delay};
+  transition-delay: ${(props) => (props.$open ? delay : "0s")};
+`;
+
+const X1 = styled(BarX)<{ $open: boolean }>`
+  transform: ${(props) => (props.$open ? "rotateZ(45deg)" : "rotateZ(0deg)")};
+`;
+
+const X2 = styled(BarX)<{ $open: boolean }>`
+  transform: ${(props) => (props.$open ? "rotateZ(-45deg)" : "rotateZ(0deg)")};
+`;
+
+function Burger({ open, toggle }: { open: boolean; toggle: () => void }) {
   return (
-    <div className={styles.Burger} onClick={() => toggle()}>
-      <div
-        className={`${styles.bar} ${styles.top} ${open ? styles.open : ""}`}
-      ></div>
-      <div
-        className={`${styles.bar} ${styles.mid} ${open ? styles.open : ""}`}
-      ></div>
-      <div
-        className={`${styles.bar} ${styles.low} ${open ? styles.open : ""}`}
-      ></div>
-      <div
-        className={`${styles.bar} ${styles.x} ${styles.x1} ${
-          open ? styles.open : ""
-        }`}
-      ></div>
-      <div
-        className={`${styles.bar} ${styles.x} ${styles.x2} ${
-          open ? styles.open : ""
-        }`}
-      ></div>
-    </div>
+    <Box onClick={() => toggle()}>
+      <TopBun $open={open} />
+      <MidBun $open={open} />
+      <LowBun $open={open} />
+      <X1 $open={open} />
+      <X2 $open={open} />
+    </Box>
   );
 }
 
