0d5ecc4ab7131b07f8f71ecf9b35aa18f425efbf
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2023-06-04 14:01:03 -0400

    Update next config to deal with styled component rendering

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  commpiler: {
+    styledComponents: true
+  }
 };
 
 module.exports = nextConfig;
